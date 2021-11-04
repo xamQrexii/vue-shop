dcd2ad32dead972e2ee2ed4cb879cc22a066d415
xamqrexii <usama1630@gmail.com>
2021-11-04 16:58:06 +0500

    refactor(remove eslint, prettier configs): remove unnecesssary formats

---
+++ src/components/Curr.vue
@@ -0,0 +1,15 @@
+<template>{{ dollar(amt) }}</template>
+
+<script>
+export default {
+  name: "Curr",
+  props: ["amt"],
+  methods: {
+    dollar(value) {
+      return `$${Number.parseFloat(value).toFixed(2)}`;
+    },
+  },
+};
+</script>
+
+<style></style>
