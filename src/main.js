2633cac80a0776593db1bd1d25d87a1a3a7d7649
Muhammad Osama <muosama@Muhammads-MacBook-Pro.local>
2021-11-18 13:53:11 +0500

    feat(store): setup basic store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -9,6 +10,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 library.add(faShoppingCart)
 
 createApp(App)
+  .use(store)
   .use(router)
   .component('fa', FontAwesomeIcon)
   .mount('#app')
