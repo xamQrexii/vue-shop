97f8985efda63be9bb2a4d668c42556530b89daf
xamqrexii <usama1630@gmail.com>
2021-11-18 20:14:43 +0500

    refactor(store): remove store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -10,7 +9,6 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 library.add(faShoppingCart)
 
 createApp(App)
-  .use(store)
   .use(router)
   .component('fa', FontAwesomeIcon)
   .mount('#app')
